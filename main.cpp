15fe558b37b48918fa4540988dabe75918b9fd84
Quan Ming Vang <mingvang2003@gmail.com>
2025-04-04 21:36:19 +0100

    Added function to generate square numbers by a random multiplier.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,20 @@
+#include "random.h"
 #include <iostream>
+#include <vector>
+
+void generateSquares(std::vector<int> &arr, int num, int start) {
+
+  int randomNum{Random::get(2, 4)};
+  for (std::size_t i{0}; i < num; ++i) {
+    arr.push_back((start * start) * randomNum);
+    start++;
+  }
+
+  std::cout << "I generated " << num
+            << " square numbers. Do you know what each number is after "
+               "multiplying it by "
+            << randomNum << "?\n";
+}
 
 int main() {
 
@@ -7,8 +23,16 @@ int main() {
   std::cin >> start;
 
   std::cout << "How many? ";
-  int generateSquares;
-  std::cin >> generateSquares;
+  int squares;
+  std::cin >> squares;
 
+  std::vector<int> squareNumsArr{};
+  generateSquares(squareNumsArr, squares, start);
+
+  for (auto &p : squareNumsArr) {
+    std::cout << p << ' ';
+  }
+
+  std::cout << '\n';
   return 0;
 }
