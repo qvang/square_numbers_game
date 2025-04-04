15fc75abcc40edca2866a2a0badbef68bd4b96d1
Quan Ming Vang <mingvang2003@gmail.com>
2025-04-04 13:21:03 +0100

    Asking for input

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+  std::cout << "Start where? ";
+  int start;
+  std::cin >> start;
+
+  std::cout << "How many? ";
+  int generateSquares;
+  std::cin >> generateSquares;
+
+  return 0;
+}
